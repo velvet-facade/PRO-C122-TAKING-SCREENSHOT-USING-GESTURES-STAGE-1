fb0fa9ab7818524682471d2e804980893a0031d9
ANANYA KANNAN <104369167+velvet-facade@users.noreply.github.com>
2023-01-17 22:09:40 +0530

    Update take_screenshot.py

--- take_screenshot.py
+++ take_screenshot.py
@@ -53,22 +53,6 @@ while True:
                 image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
                 cv2.imwrite("in_memory_to_disk.png", image)
 
-                # this time take a screenshot directly to disk
-                gui.screenshot("straight_to_disk.png")
-
-                # we can then load our screenshot from disk in OpenCV format
-                image = cv2.imread("straight_to_disk.png")
-                cv2.imshow("Screenshot", imutils.resize(image, width=600))
-
-            mp_draw.draw_landmarks(img, hand_landmark,
-            mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0,0,255),2,2),
-            mp_draw.DrawingSpec((0,255,0),4,2))
-    
-
-    cv2.imshow("hand tracking", img)
-    cv2.waitKey(1)
-
-
 
 
 
